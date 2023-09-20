1646a2fe5a7702bf586251f647c4ab559d5ad614
LesterSerrano <lesterserano66@gmail.com>
2023-09-20 09:39:44 -0600

    Empleado service, clases de la DAL en SERVIS

---
+++ ESFE.GestorTareas.BL/Service/IEmpleadoService.cs
@@ -0,0 +1,20 @@
+﻿using ESFE.GestorTareas.EN;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESFE.GestorTareas.BL.Service
+{
+    public interface IEmpleadoService
+    {
+        Task<bool> Insertar(Empleado modelo);
+        Task<bool> Actualizar(Empleado modelo);
+        Task<bool> Eliminar(int id);
+        Task<Empleado> Obtener(int id);
+        Task<IQueryable<Empleado>> ObtenerTodos();
+
+        Task<Empleado> ObtenerPorNombre(string nombreTarea);
+    }
+}
--- ESFE.GestorTareas.BL/Service/IUsuarioService.cs
+++ ESFE.GestorTareas.BL/Service/IUsuarioService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ESFE.GestorTareas.EN;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,12 @@ namespace ESFE.GestorTareas.BL.Service
 {
     public interface IUsuarioService
     {
+        Task<bool> Insertar(Usuario modelo);
+        Task<bool> Actualizar(Usuario modelo);
+        Task<bool> Eliminar(int id);
+        Task<Usuario> Obtener(int id);
+        Task<IQueryable<Usuario>> ObtenerTodos();
 
+        Task<Usuario> ObtenerPorNombre(string nombreUsuario);
     }
 }
