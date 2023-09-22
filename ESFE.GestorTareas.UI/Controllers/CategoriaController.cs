cebba81497839d29075ff53530a216b9beb31f33
Jeffrey Mardoqueo <jeffreymardoqueo260@gmail.com>
2023-09-21 21:25:32 -0600

    Los controladores y las VIEWs

---
+++ ESFE.GestorTareas.UI/Controllers/CategoriaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ESFE.GestorTareas.UI.Controllers
+{
+    public class CategoriaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ESFE.GestorTareas.UI/Controllers/TareaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ESFE.GestorTareas.UI.Controllers
+{
+    public class TareaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
