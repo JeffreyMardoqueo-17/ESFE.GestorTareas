0fed6dda2cb87613badeffa63b42206c904a76b3
Jeffrey Mardoqueo <jeffreymardoqueo260@gmail.com>
2023-09-25 12:58:44 -0600

    Problemas

---
+++ ESFE.GestorTareas.UI/wwwroot/js/Tarea.js
@@ -0,0 +1,28 @@
+﻿alert("ESTO FUNCIONA")
+
+//MODELO BASE, de donde se tomara como mold
+const Modelo_base = {
+    id: 0,
+    nombre: "",
+    Descripcion: "",
+    FechaVencimiento: "",
+    Categoria: "",
+    Prioridad: "",
+    EstadoTarea: "",
+    Asignacion: "",
+    Categoria: "",
+    Estado: "",
+    Prioridad: "",
+}
+
+const mostrarModal = (m) => {
+
+    $("#txtId").val(m.id);
+    $("#txtNombre").val(m.nombre)
+
+    $('.modal').modal('show');
+}
+$("#btnNuevo").click(() => {
+
+    mostrarModal(Modelo_base);
+});
