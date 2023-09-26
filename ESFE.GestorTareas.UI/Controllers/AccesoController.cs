dd90fa06ef1d678558971553ab60d4713bb70659
LesterSerrano <lesterserano66@gmail.com>
2023-09-26 11:42:53 -0600

    Cambios

--- ESFE.GestorTareas.UI/Controllers/AccesoController.cs
+++ ESFE.GestorTareas.UI/Controllers/AccesoController.cs
@@ -1,7 +1,10 @@
 ﻿using ESFE.GestorTareas.BL.Service;
 using ESFE.GestorTareas.EN;
 using ESFE.GestorTareas.UI.Models.ViewModels;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace ESFE.GestorTareas.UI.Controllers
@@ -15,12 +18,6 @@ namespace ESFE.GestorTareas.UI.Controllers
             _usuarioService = usuarioService;
         }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        [HttpGet]
         public IActionResult Registro()
         {
             return View();
@@ -29,36 +26,49 @@ namespace ESFE.GestorTareas.UI.Controllers
         [HttpPost]
         public async Task<IActionResult> Registro(VMUsuario modelo)
         {
-            if (ModelState.IsValid)
-            {
-                // Verifica si el correo electrónico ya está registrado
-                if (await _usuarioService.ExisteCorreo(modelo.Correo))
-                {
-                    ModelState.AddModelError("Correo", "El correo electrónico ya está registrado.");
-                }
-                else
-                {
-                    var usuarioRegistrado = await _usuarioService.Insertar(new Usuario
-                    {
-                        Nombre = modelo.Nombre,
-                        Correo = modelo.Correo,
-                        Pass = modelo.Pass,
-                    });
+            Usuario UsuarioRegistrado = await _usuarioService.Insertar(modelo);
 
-                    if (usuarioRegistrado)
-                    {
-                        return RedirectToAction("Index", "Acceso");
-                    }
-                    else
-                    {
-                        // Hubo un error al registrar al usuario
-                        ModelState.AddModelError(string.Empty, "Hubo un error al registrar el usuario.");
-                    }
-                }
+            if (UsuarioRegistrado.Id > 0)
+                return RedirectToAction("IniciarSesion", "Inicio");
+
+            ViewData["Mensaje"] = "No se pudo crear el usuario";
+            return View();
+        }
+
+        public IActionResult IniciarSesion()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> IniciarSesion(string correo, string clave)
+        {
+
+            Usuario usuario_encontrado = await _usuarioService.GetUsuario(correo, Utilidades.EncriptarClave(clave));
+
+            if (usuario_encontrado == null)
+            {
+                ViewData["Mensaje"] = "No se encontraron coincidencias";
+                return View();
             }
 
-            // Si el modelo no es válido o hubo un error, muestra la vista de registro nuevamente
-            return View(modelo);
+            List<Claim> claims = new List<Claim>() {
+                new Claim(ClaimTypes.Name, usuario_encontrado.NombreUsuario)
+            };
+
+            ClaimsIdentity claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+            AuthenticationProperties properties = new AuthenticationProperties()
+            {
+                AllowRefresh = true
+            };
+
+            await HttpContext.SignInAsync(
+                CookieAuthenticationDefaults.AuthenticationScheme,
+                new ClaimsPrincipal(claimsIdentity),
+                properties
+                );
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
