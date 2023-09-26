03e7b6f674c1f63da49e69b7cb2a43d131bf6220
Jeffrey Mardoqueo <jeffreymardoqueo260@gmail.com>
2023-09-26 08:05:28 -0600

    Seccion de tarea

--- ESFE.GestorTareas.UI/wwwroot/js/Tarea.js
+++ ESFE.GestorTareas.UI/wwwroot/js/Tarea.js
@@ -1,6 +1,5 @@
 ﻿alert("ESTO FUNCIONA")
 
-<<<<<<< HEAD
 //MODELO BASE, de donde se tomara como mold
 const Modelo_base = {
     id: 0,
@@ -14,22 +13,23 @@ const Modelo_base = {
     Categoria: "",
     Estado: "",
     Prioridad: "",
-=======
 //MODELO BASE
-const Modelo_base = {
+const modeloBase = {
     id: 0,
     nombre: "",
->>>>>>> 0970cf533122e8cb9779090cced7c9a79a175a6b
 }
 
 const mostrarModal = (m) => {
 
     $("#txtId").val(m.id);
-    $("#txtNombre").val(m.nombre)
+    $("#txtNombre").val(m.nombre);
+    $("#txtTDesc").val(m.Descripcion);
+    $("#dtFecha").val(m.FechaVencimiento);
+    $("#cbxCategoria").val(m.Categoria);
 
     $('.modal').modal('show');
 }
 $("#btnNuevo").click(() => {
 
-    mostrarModal(Modelo_base);
+    mostrarModal(modeloBase);
 });
---
+++ ESFE.GestorTareas.UI/wwwroot/js/Tareaa.js
@@ -0,0 +1,35 @@
+﻿alert("ESTO FUNCIONA")
+
+//MODELO BASE, de donde se tomara como mold
+const Modelo_base = {
+    id: 0,
+    nombre: "",
+    Descripcion: "",
+    FechaVencimiento: "",
+    Categoria: "",
+    Prioridad: "",
+    EstadoTarea: "",
+    Asignacion: "",
+    Categoria: "",
+    Estado: "",
+    Prioridad: "",
+    //MODELO BASE
+    const modeloBase = {
+        id: 0,
+        nombre: "",
+    }
+
+const mostrarModal = (m) => {
+
+        $("#txtId").val(m.id);
+        $("#txtNombre").val(m.nombre);
+        $("#txtTDesc").val(m.Descripcion);
+        $("#dtFecha").val(m.FechaVencimiento);
+        $("#cbxCategoria").val(m.Categoria);
+
+        $('.modal').modal('show');
+    }
+$("#btnNuevo").click(() => {
+
+        mostrarModal(modeloBase);
+    });
